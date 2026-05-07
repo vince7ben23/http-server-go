eef86ca69db32c3d4918ba122b064225e6823590
benben <vince7ben23@gmail.com>
2026-05-07 12:48:31 +0800

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -9,19 +9,36 @@ import (
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	// fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	
+
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	_, err = l.Accept()
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	buff := make([]byte, 1024)
+
+	n, err := conn.Read(buff)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+		return
+	}
+	// fmt.Println("n:", n)
+	fmt.Println("Received:", string(buff[0:n]))
+
+	response := "HTTP/1.1 200 OK\r\n" +
+		// "Content-Length: 6\r\n" +
+		// "Content-Type: text/plain\r\n" +
+		"\r\n" +
+		""
+	conn.Write([]byte(response))
 }
